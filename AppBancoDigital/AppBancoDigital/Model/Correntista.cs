10c6d74cd9d51594732e39922f8114e3b94a48e1
fernandotrevisan <fernando.trevisan01@etec.sp.gov.br>
2023-07-27 14:55:33 -0300

    Atualizando !!

--- AppBancoDigital/AppBancoDigital/Model/Correntista.cs
+++ AppBancoDigital/AppBancoDigital/Model/Correntista.cs
@@ -6,11 +6,11 @@ namespace AppBancoDigital.Model
 {
     public class Correntista
     {
-
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Nome { get; set; }
-        public DateTime Data_Nasc { get; set; }
-        public int Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+        public string Cpf { get; set; }
         public string Senha { get; set; }
 
 
